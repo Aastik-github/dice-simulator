e769c97a5148a50f4fd71ca61f7d1b221531484d
aastik tripathi <83272032+Aastik-github@users.noreply.github.com>
2021-07-28 15:39:29 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+import random 
+print("dice by aastik")
+a = [1,2,3,4,5,6] 
+b = random.choice(a) # assiging the variable b to a random choice made by the computer from the list a.
+print(f"your number is: {b}")
